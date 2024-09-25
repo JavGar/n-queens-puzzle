f84320822a31c75f292c6c18744b8fbe2b345c1c
JavGar <dushtajavier@hotmail.com>
2024-09-24 19:05:57 -0600

    feat: manage duplicated solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from sqlalchemy.sql import insert
+from sqlalchemy.dialects.postgresql import insert as pg_insert
 from sqlalchemy.exc import SQLAlchemyError
 from db.db import database, solutions_table
 from utils.n_queens_puzzle import get_n_queens_puzzle_solutions
@@ -14,11 +14,13 @@ def read_root():
 async def get_puzzle_solutions(n: int = 1):
     solutions = get_n_queens_puzzle_solutions(n)
     try:
-        query = insert(solutions_table).values(n_queens = n, solutions = solutions)
+        query = pg_insert(solutions_table).values(n_queens = n, solutions = solutions)
+        query = query.on_conflict_do_nothing(index_elements=['n_queens'])
         await database.execute(query)
-        return {"message": "Solutions retrieved", "n": n, "solutions": solutions}
     except SQLAlchemyError as e:
         raise HTTPException(Status_code=500, detail=str(e))
+
+    return {"message": "Solutions retrieved", "n": n, "solutions": solutions}
 
 @app.on_event("startup")
 async def startup():
