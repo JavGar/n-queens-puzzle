f721c489745d712a14d1010f216bcbb0074f5703
JavGar <dushtajavier@hotmail.com>
2024-09-24 18:11:54 -0600

    feat: add endpoint to get puzzle solutions

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from utils.n_queens_puzzle import get_n_queens_puzzle_solutions
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/puzzle")
+async def get_puzzle_solutions(n: int = 1):
+    return {"message": "Solutions retrieved", "n": n, "solutions": get_n_queens_puzzle_solutions(n)}
