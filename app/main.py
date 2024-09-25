19d3faf24f69052fcaeff6153aaba2b829a9d548
JavGar <dushtajavier@hotmail.com>
2024-09-24 19:13:33 -0600

    feat: implement get solutions stored

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI, HTTPException
+from sqlalchemy import select, asc
 from sqlalchemy.dialects.postgresql import insert as pg_insert
-from sqlalchemy.exc import SQLAlchemyError
 from db.db import database, solutions_table
 from utils.n_queens_puzzle import get_n_queens_puzzle_solutions
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+async def read_root():
+    try:
+        query = select(solutions_table).order_by(solutions_table.c.n_queens)
+        results = await database.fetch_all(query)
+        return {"message": "All solutions retrieved", "solutions": results}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 @app.get("/puzzle")
 async def get_puzzle_solutions(n: int = 1):
@@ -17,7 +22,7 @@ async def get_puzzle_solutions(n: int = 1):
         query = pg_insert(solutions_table).values(n_queens = n, solutions = solutions)
         query = query.on_conflict_do_nothing(index_elements=['n_queens'])
         await database.execute(query)
-    except SQLAlchemyError as e:
+    except Exception as e:
         raise HTTPException(Status_code=500, detail=str(e))
 
     return {"message": "Solutions retrieved", "n": n, "solutions": solutions}
