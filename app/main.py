77289eb592c74b328ebbc77f3ba543753dc5ff17
JavGar <dushtajavier@hotmail.com>
2024-09-26 18:18:22 -0600

    chore: add docs

--- app/main.py
+++ app/main.py
@@ -1,14 +1,25 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from sqlalchemy import select, asc
 from sqlalchemy.dialects.postgresql import insert as pg_insert
 from db.db import database, get_n_queens_puzzle_stored_solutions, solutions_table
 from utils.n_queens_puzzle import get_n_queens_puzzle_solutions
 
-app = FastAPI()
+app = FastAPI(
+    title="N-Queens Puzzle Solver",
+    description="API to solve the N-Queens Puzzle using FastAPI.",
+    version="1.0.0",
+)
 
 
 @app.get("/")
-async def read_root():
+async def get_solutions():
+    """
+    Retrieve all stored N-Queens solutions from the database.
+
+    Returns:
+        message: response message
+        solutions: list of all N-Queens solutions.
+    """
     try:
         query = select(solutions_table).order_by(solutions_table.c.n_queens)
         results = await database.fetch_all(query)
@@ -18,7 +29,20 @@ async def read_root():
 
 
 @app.get("/puzzle")
-async def get_puzzle_solutions(n: int = 1):
+async def get_puzzle_solutions(
+    n: int = Query(1, description="The size of the N-Queens board")
+):
+    """
+    Get solution for a specific N-Queens board size.
+
+    Args:
+        n: The size of the N-Queens board.
+
+    Returns:
+        message: response message
+        n: board size
+        solutions: list of all N-Queens solutions.
+    """
     solutions = await get_n_queens_puzzle_stored_solutions(n)
     if solutions:
         return {"message": "Solutions retrieved", "n": n, "solutions": solutions}
